745ce9f79ca8b5860efac8abee6bb940d38191d8
Arun Vishwakarma <106154027+ArunVishwakarma30@users.noreply.github.com>
2025-02-21 10:51:19 +0530

    Update long_btn.js appostrohpe

--- src/app/components/home_page_comps.js/long_btn.js
+++ src/app/components/home_page_comps.js/long_btn.js
@@ -7,7 +7,7 @@ export default function LongBtn() {
             rel="noopener noreferrer"
         >
             <button className="rounded-full border-2 border-[#2b6cb0] bg-[#edf2f7] text-xs text-[#1a202] font-semibold px-4 py-2 text-sm shadow-sm hover:bg-blue-100 focus:outline-none  focus:ring-[#2b6cb0] focus:ring-offset-2">
-                Hello, I'm a Mobile App Developer based in India
+                Hello, I&apos;m a Mobile App Developer based in India
             </button>
         </a>
     );
